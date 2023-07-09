70986c22057c845d58a75efc16fb899be5d77826
skykbw <68388295+skykbw@users.noreply.github.com>
2023-07-09 16:18:02 +0900

    Update GildedRoseTest.java

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
+    public void should_be_nothing_when_no_item(){
+        Item[] items = new Item[]{};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQualityItems();
+
+        assertEquals(0,items.length);
+    }
+
+    @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        app.updateQualityItems();
+        assertEquals("foo", app.items[0].name);
     }
 
 }
