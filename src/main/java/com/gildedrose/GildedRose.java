ea43c98279863c474d27ccc843aa86b7cdfe2b1c
skykbw@naver.com <skykbw@naver.com>
2023-07-09 13:56:59 +0900

    refactor : split if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,33 +13,50 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (item.name.equals(AGED_BRIE)
-                || item.name.equals(BACKSTAGE_PASS)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                        if (item.name.equals(BACKSTAGE_PASS)) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
+                    if (item.name.equals(BACKSTAGE_PASS)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
+                        }
 
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
-                } else {
-            if (item.quality > 0) {
-                if (item.name.equals(SULFRURAS)) {
-                } else {
-                    item.quality = item.quality - 1;
+                }
+            } else if (item.name.equals(BACKSTAGE_PASS)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.name.equals(BACKSTAGE_PASS)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (item.quality > 0) {
+                    if (item.name.equals(SULFRURAS)) {
+                    } else {
+                        item.quality = item.quality - 1;
+                    }
                 }
             }
-        }
 
             if (item.name.equals(SULFRURAS)) {
             } else {
