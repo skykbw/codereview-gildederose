c38856f415188d0861fb190dc834a04d23833894
skykbw@naver.com <skykbw@naver.com>
2023-07-09 13:56:17 +0900

    refactor : Convert if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,51 +13,54 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASS)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFRURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            if (item.name.equals(AGED_BRIE)
+                || item.name.equals(BACKSTAGE_PASS)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGE_PASS)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                        if (item.name.equals(BACKSTAGE_PASS)) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
                             }
-                        }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
                             }
                         }
                     }
+                } else {
+            if (item.quality > 0) {
+                if (item.name.equals(SULFRURAS)) {
+                } else {
+                    item.quality = item.quality - 1;
                 }
             }
+        }
 
-            if (!item.name.equals(SULFRURAS)) {
+            if (item.name.equals(SULFRURAS)) {
+            } else {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASS)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFRURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.name.equals(AGED_BRIE)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE_PASS)) {
+                        item.quality = item.quality - item.quality;
+                    } else {
+                        if (item.quality > 0) {
+                            if (item.name.equals(SULFRURAS)) {
+                                continue;
+                            }
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
             }
